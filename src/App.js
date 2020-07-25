777f19f85c8ea5cc9c0de2b0aa70e35e6cf553a9
aschmitt80 <a.schmitt.80@gmail.com>
2020-07-25 18:05:01 +0200

    added feature

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Hi there!!!!!<br></br>To get started, edit <code>src/App.js</code> and save to reload.
+          feature: Hi there!!!!!<br></br>To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
